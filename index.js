4e6c418c32dbc3e1428c420eb754d7911d6eff87
liam <liam@nehoray.net>
2024-09-29 16:46:42 +0200

    finished project

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const fs = require('node:fs/promises');
+import http from "http";
+import fs from 'node:fs'
 
-const EventEmitter = require('node:events');
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const directoryPath = '/home/liam/odin-project/basic-information-page'
 
-const eventEmitter = new EventEmitter();
+    let HTML
+    switch (req.url) {
+        case '/':
+            HTML = getHTML(directoryPath + '/index.html');
+            res.write(HTML)
 
-eventEmitter.on('', () => {
-    console.log('started')
-})
+            break;
+        case '/contact':
+            HTML = getHTML(directoryPath + '/contact.html');
+            res.write(HTML)
 
-eventEmitter.emit('start');
+            break;
+        case '/about':
+            HTML = getHTML(directoryPath + '/about.html');
+            res.write(HTML)
+
+            break;
+        default:
+            HTML = getHTML(directoryPath + '/404.html');
+            res.write(HTML)
+            break;
+
+    }
+
+
+    res.end();
+}).listen('8080')
+
+function getHTML(filePath) {
+    console.log(filePath)
+    try {
+        const result = fs.readFileSync(filePath, 'utf-8')
+        return result
+    } catch (err) {
+        console.log(err)
+    }
+}
